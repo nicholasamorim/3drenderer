e9e8c370b2944ea972e0be301e1fd2673f8add88
Nicholas Amorim <nicholas@santos.ee>
2025-04-26 01:50:06 +0300

    Add color buffer and fill it with yellow.

--- src/main.c
+++ src/main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdbool.h>
 #include <SDL2/SDL.h>
 
 bool is_running;
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
+
+uint32_t* color_buffer = NULL;
+SDL_Texture* color_buffer_texture = NULL; // used to display color buffer
+
+int window_width = 800;
+int window_height = 600;
 
 bool initialize_window(void) {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
@@ -16,8 +23,8 @@ bool initialize_window(void) {
         NULL,
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        800,
-        600,
+        window_width,
+        window_height,
         SDL_WINDOW_BORDERLESS
     );
     if (!window) {
@@ -35,7 +42,27 @@ bool initialize_window(void) {
 }
 
 void setup(void) {
+    // Allocate required bytes for the color buffer
+    color_buffer = (uint32_t*) malloc(sizeof(uint32_t) * window_width * window_height);
+    
+    // Create a SDL Texture for the color display
+    color_buffer_texture = SDL_CreateTexture(
+        renderer,
+        SDL_PIXELFORMAT_ARGB8888,
+        SDL_TEXTUREACCESS_STREAMING,
+        window_width,
+        window_height
+    );
 
+    // if (!color_buffer) {
+    // not successful
+    // }
+
+    // Set first pixel to red
+    color_buffer[0] = 0xFFFF0000;
+    color_buffer[1] = 0xFF00FF00;
+    // Row 10, column 20 to red
+    color_buffer[(window_width * 10) + 20] = 0xFFFF0000;
 }
 
 
@@ -58,14 +85,44 @@ void update(void) {
 
 }
 
+void render_color_buffer() {
+    // Update texture rectangle with new pixel data
+    SDL_UpdateTexture(
+        color_buffer_texture,
+        NULL,
+        color_buffer,
+        (int)(window_width * sizeof(uint32_t))
+    );
+    
+    SDL_RenderCopy(renderer, color_buffer_texture, NULL, NULL);
+}
+
+
+void clear_color_buffer(uint32_t color) {
+    for (int y = 0; y < window_height; y++) {
+        for (int x = 0; x < window_width; x++) {
+            color_buffer[(window_width * y) + x] = color;
+        }
+    }
+}
+
 
 void render(void) {
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_SetRenderDrawColor(renderer, 1, 0, 0, 255);
     SDL_RenderClear(renderer);
+
+    render_color_buffer();
+    clear_color_buffer(0xFFFFFF00);
 
     SDL_RenderPresent(renderer);
 }
 
+void destroy_window(void) {
+    free(color_buffer);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
 
 int main(void) {
     is_running = initialize_window();
@@ -77,6 +134,8 @@ int main(void) {
         update();
         render();
     }
+
+    destroy_window();
 
     return 0;
 }
