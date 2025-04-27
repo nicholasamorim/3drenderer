4efa80cbca74e1627550cc5c6fee77367e45eea6
Nicholas Amorim <nicholas@santos.ee>
2025-04-27 23:44:10 +0300

    Basic vector operations

--- src/vector.c
+++ src/vector.c
@@ -28,3 +28,49 @@ vec3_t vec3_rotate_x(vec3_t v, float angle) {
     };
     return rotated_vector;
 }
+
+float vec2_length(vec2_t v) {
+    return sqrt(pow(v.x, 2) + pow(v.y, 2));
+}
+
+float vec3_length(vec3_t v) {
+    return sqrt(pow(v.x, 2) + pow(v.y, 2) + pow(v.z, 2));
+}
+
+vec2_t vec2_add(vec2_t v1, vec2_t v2) {
+    vec2_t vec = {
+        v1.x + v2.x,
+        v1.y + v2.y,
+    };
+
+    return vec;
+}
+
+vec3_t vec3_add(vec3_t v1, vec3_t v2) {
+    vec3_t vec = {
+        v1.x + v2.x,
+        v1.y + v2.y,
+        v1.z + v2.z,
+    };
+
+    return vec;
+}
+
+vec2_t vec2_sub(vec2_t v1, vec2_t v2) {
+    vec2_t vec = {
+        v1.x - v2.x,
+        v1.y - v2.y,
+    };
+
+    return vec;
+}
+
+vec3_t vec3_sub(vec3_t v1, vec3_t v2) {
+    vec3_t vec = {
+        v1.x - v2.x,
+        v1.y - v2.y,
+        v1.z - v2.z,
+    };
+
+    return vec;
+}
--- src/vector.h
+++ src/vector.h
@@ -2,15 +2,12 @@
 #define VECTOR_H
 
 typedef struct {
-    float x;
-    float y;
+    float x, y;
 } vec2_t;
 
 
 typedef struct {
-    float x;
-    float y;
-    float z;
+    float x, y, z;
 } vec3_t;
 
 typedef struct {
@@ -23,4 +20,14 @@ typedef struct {
 vec3_t vec3_rotate_z(vec3_t v, float angle);
 vec3_t vec3_rotate_y(vec3_t v, float angle);
 vec3_t vec3_rotate_x(vec3_t v, float angle);
+
+float vec2_length(vec2_t v);
+float vec3_length(vec3_t v);
+
+vec2_t vec2_add(vec2_t v1, vec2_t v2);
+vec3_t vec3_add(vec3_t v1, vec3_t v2);
+
+vec2_t vec2_sub(vec2_t v1, vec2_t v2);
+vec3_t vec3_sub(vec3_t v1, vec3_t v2);
+
 #endif
