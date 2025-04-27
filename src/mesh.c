fc46bb0ab4e0c863b8a2705cf190dd417e44b097
Nicholas Amorim <nicholas@santos.ee>
2025-04-27 14:31:05 +0300

    Basic parsing/loading of obj files

--- src/mesh.c
+++ src/mesh.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "array.h"
 #include "mesh.h"
 
@@ -58,9 +59,35 @@ void load_obj_file_data(char* filename) {
         fprintf(stderr, "Cannot read asset\n");
         return;
     }
-    char line[1024];
+    char buffer[1024];
 
-    while (fgets(line, 1024, file)) {
+    while (fgets(buffer, sizeof(buffer), file)) {
 
+        if (strncmp(buffer, "v ", 2) == 0) {
+            vec3_t vertex;
+            sscanf(buffer, "v %f %f %f", &vertex.x, &vertex.y, &vertex.z);
+            array_push(mesh.vertices, vertex);
+        } else if (strncmp(buffer, "f ", 2) == 0) {
+            // structure to store the f lines from the obj
+            int vertex_indices[3];
+            int texture_indices[3];
+            int normal_indices[3];
+            
+            sscanf(
+                buffer, 
+                "f %d/%d/%d %d/%d/%d %d/%d/%d",
+                &vertex_indices[0], &texture_indices[0], &normal_indices[0], 
+                &vertex_indices[1], &texture_indices[1], &normal_indices[1], 
+                &vertex_indices[2], &texture_indices[2], &normal_indices[2] 
+            );
+
+            face_t face = {
+                .a = vertex_indices[0],
+                .b = vertex_indices[1],
+                .c = vertex_indices[2],
+            };
+
+            array_push(mesh.faces, face);
+        }
     }
 }
