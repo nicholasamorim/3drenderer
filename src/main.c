9b1e018a0c6f928532b2692c6cbca63ba41dbe82
Nicholas Amorim <nicholas@santos.ee>
2025-04-30 02:42:00 +0300

    Using translation matrix

--- src/main.c
+++ src/main.c
@@ -147,10 +147,12 @@ void update(void) {
     mesh.rotation.x += 0.01;
     mesh.rotation.y += 0.01;
     mesh.rotation.z += 0.01;
-
+    mesh.translation.x += 0.01;
+    mesh.translation.z = 5.0;
     mesh.scale.x += 0.0002;
 
     mat4_t scale_matrix = mat4_make_scale(mesh.scale.x, mesh.scale.y, mesh.scale.z);
+    mat4_t translation_matrix = mat4_make_translation(mesh.translation.x, mesh.translation.y, mesh.translation.z);
 
     // Loop over triangle faces
     int num_faces = array_length(mesh.faces);
@@ -169,8 +171,7 @@ void update(void) {
             vec4_t transformed_vertex = vec4_from_vec3(face_vertices[j]);
 
             transformed_vertex = mat4_mul_vec4(scale_matrix, transformed_vertex);
-
-            transformed_vertex.z += 5;
+            transformed_vertex = mat4_mul_vec4(translation_matrix, transformed_vertex);
 
             transformed_vertices[j] = transformed_vertex;
         }
