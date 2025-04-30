4d881409550b6f09c6a94f4de8337f32fcaa762b
Nicholas Amorim <nicholas@santos.ee>
2025-04-30 03:47:36 +0300

    Using world matrix

--- src/main.c
+++ src/main.c
@@ -149,7 +149,8 @@ void update(void) {
     mesh.rotation.z += 0.01;
     mesh.translation.x += 0.01;
     mesh.translation.z = 5.0;
-    // mesh.scale.x += 0.0002;
+    mesh.scale.x += 0.0002;
+    mesh.scale.y += 0.0001;
 
     mat4_t scale_matrix = mat4_make_scale(mesh.scale.x, mesh.scale.y, mesh.scale.z);
     mat4_t translation_matrix = mat4_make_translation(mesh.translation.x, mesh.translation.y, mesh.translation.z);
@@ -174,11 +175,20 @@ void update(void) {
         for (int j = 0; j < 3; j++) {
             vec4_t transformed_vertex = vec4_from_vec3(face_vertices[j]);
 
-            transformed_vertex = mat4_mul_vec4(scale_matrix, transformed_vertex);
-            transformed_vertex = mat4_mul_vec4(rotation_matrix_x, transformed_vertex);
-            transformed_vertex = mat4_mul_vec4(rotation_matrix_y, transformed_vertex);
-            transformed_vertex = mat4_mul_vec4(rotation_matrix_z, transformed_vertex);
-            transformed_vertex = mat4_mul_vec4(translation_matrix, transformed_vertex);
+            mat4_t world_matrix = mat4_identity();
+            world_matrix = mat4_mul_mat4(scale_matrix, world_matrix);
+            world_matrix = mat4_mul_mat4(rotation_matrix_z, world_matrix);
+            world_matrix = mat4_mul_mat4(rotation_matrix_y, world_matrix);
+            world_matrix = mat4_mul_mat4(rotation_matrix_x, world_matrix);
+            world_matrix = mat4_mul_mat4(translation_matrix, world_matrix);
+            
+            transformed_vertex = mat4_mul_vec4(world_matrix, transformed_vertex);
+
+            // transformed_vertex = mat4_mul_vec4(scale_matrix, transformed_vertex);
+            // transformed_vertex = mat4_mul_vec4(rotation_matrix_x, transformed_vertex);
+            // transformed_vertex = mat4_mul_vec4(rotation_matrix_y, transformed_vertex);
+            // transformed_vertex = mat4_mul_vec4(rotation_matrix_z, transformed_vertex);
+            // transformed_vertex = mat4_mul_vec4(translation_matrix, transformed_vertex);
 
             transformed_vertices[j] = transformed_vertex;
         }
--- src/matrix.h
+++ src/matrix.h
@@ -8,11 +8,14 @@ typedef struct {
 } mat4_t;
 
 mat4_t mat4_identity(void);
+
 mat4_t mat4_make_scale(float sx, float sy, float sz);
 mat4_t mat4_make_translation(float tx, float ty, float tz);
-vec4_t mat4_mul_vec4(mat4_t m, vec4_t v);
 mat4_t mat4_make_rotation_z(float angle);
 mat4_t mat4_make_rotation_x(float angle);
 mat4_t mat4_make_rotation_y(float angle);
+
+vec4_t mat4_mul_vec4(mat4_t m, vec4_t v);
+mat4_t mat4_mul_mat4(mat4_t a, mat4_t b);
 
 #endif
